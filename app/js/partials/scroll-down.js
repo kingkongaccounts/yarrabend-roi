44bcd03797d7276b6d631b2f574de7ac1bd1ad8c
Jolane Synott <jolane@jolane.net>
2016-06-14 16:37:39 +1000

    Added responsive break points.

--- app/js/partials/scroll-down.js
+++ app/js/partials/scroll-down.js
@@ -1,12 +1,15 @@
 jQuery(document).ready(function($) {
 	$('.js-scroll-down-1').click(function(event) {
 		event.preventDefault();
-
-		// var currentScroll = $(window).scrollTop();
-		// var windowHeight = $(window).height();
-		// var newScroll = currentScroll + windowHeight;
-
 		$('body, html').animate({scrollTop : $(window).height() }, 400);
+
+	});
+
+	$('.js-scroll-down-2').click(function(event) {
+		event.preventDefault();
+		var h = $(window).height() * 2;
+
+		$('body, html').animate({scrollTop : h }, 400);
 
 	});
 });
