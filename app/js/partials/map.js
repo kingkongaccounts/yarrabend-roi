eec48a268b781384dadb6e77d9d6dba2258b4026
Jolane Synott <jolane@jolane.net>
2016-06-16 12:57:34 +1000

    Updated YB marker location, changed Future Focused image.

--- app/js/partials/map.js
+++ app/js/partials/map.js
@@ -1,7 +1,7 @@
 var map;
 function initMap() {
 
-	var myLatLng = {lat: -37.781054, lng: 145.030695};
+	var myLatLng = {lat: -37.781167, lng: 145.029913};
 	var styles = [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]}];
 	var markers = [];
 	var infowindows = [];
