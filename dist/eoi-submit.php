1cce9305c8af752bf6e7075e7f5ed024f755f489
Jolane Synott <jolane.synott@CWL58-JolaneSynott-MacBook-Pro.local>
2016-07-28 09:05:38 +1000

    Added email function to EOI submit.

--- dist/eoi-submit.php
+++ dist/eoi-submit.php
@@ -43,6 +43,40 @@ if(filter_var($_POST['applicant1_email'], FILTER_VALIDATE_EMAIL)) {
 	$mysqli->query($query);
 
 
+	// send email
+	$to = 'jolane.synott@cornwell.com.au';
+	$subject = "New EOI from YarraBend.";
+	$headers = 'From: no-reply@yarrabend.com.au' . "\r\n";
+	$message = "
+applicant1_title: $applicant1_title \n
+applicant1_first_name: $applicant1_first_name \n
+applicant1_last_name: $applicant1_last_name \n
+applicant1_email: $applicant1_email \n
+applicant1_phone: $applicant1_phone \n
+applicant1_street: $applicant1_street \n
+applicant1_city: $applicant1_city \n
+applicant1_postcode: $applicant1_postcode \n
+applicant1_state: $applicant1_state \n
+applicant1_country: $applicant1_country \n
+applicant2: $applicant2 \n
+applicant2_first_name: $applicant2_first_name \n
+applicant2_last_name: $applicant2_last_name \n
+applicant2_email: $applicant2_email \n
+applicant2_phone: $applicant2_phone \n
+applicant2_street: $applicant2_street \n
+applicant2_city: $applicant2_city \n
+applicant2_postcode: $applicant2_postcode \n
+applicant2_state: $applicant2_state \n
+applicant2_country: $applicant2_country \n
+identification: $identification \n
+product1: $product1 \n
+product2: $product2 \n
+product3: $product3 \n
+";
+
+	mail($to, $subject, $message, $headers);
+
+
 	header("location: eoi-thankyou.html");
 	exit();
 
